b09b7353e77a95e3887b8598cd4a2faa6d7956d7
Le Thai Thanh <admin@lethai.vn>
2022-05-19 22:08:01 +0700

    Create frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
